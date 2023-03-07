eeae9dc066b92636c57c5fb5eda93b64d4acf8ca
minhtien41001 <tranminhtien41001@gmail.com>
2023-03-07 15:30:10 +0700

    up

---
+++ ss03_angular/calculator/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  num1 = 0;
+  num2 = 0;
+  result : any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getAddition(){
+    this.result = this.num1 + this.num2;
+  }
+
+  getSubtraction(){
+    this.result = this.num1 - this.num2;
+  }
+
+  getMultiplication(){
+    this.result = this.num1 * this.num2;
+  }
+
+  getDivision(){
+    if (this.num2 == 0){
+      this.result = "Không thể chia cho 0!"
+    }else {
+      this.result = this.num1 / this.num2;
+    }
+  }
+}
