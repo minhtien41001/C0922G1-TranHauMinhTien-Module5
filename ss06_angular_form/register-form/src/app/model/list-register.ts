6d994c20e85988f11852aff98535f1b077b45db9
minhtien41001 <tranminhtien41001@gmail.com>
2023-03-10 19:35:24 +0700

    update

---
+++ ss06_angular_form/register-form/src/app/model/list-register.ts
@@ -0,0 +1,9 @@
+export interface ListRegister {
+  email?: string
+  password?: string
+  confirmPassword?: string
+  country?: string
+  age?: number
+  gender?: string
+  phone?: number
+}
---
+++ ss06_angular_form/register-form/src/app/register-component/register-component.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
+import {ListRegister} from "../model/list-register";
+
+@Component({
+  selector: 'app-register-component',
+  templateUrl: './register-component.component.html',
+  styleUrls: ['./register-component.component.css']
+})
+export class RegisterComponentComponent implements OnInit {
+  rfRegister: FormGroup;
+  listRegister: ListRegister [] = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.rfRegister = new FormGroup({
+      email: new FormControl("",[Validators.required,Validators.email]),
+      password: new FormControl("",[Validators.required,Validators.minLength(6)]),
+      confirmPassword: new FormControl("",[Validators.required,Validators.minLength(6),this.checkPass]),
+      country: new FormControl("",[Validators.required]),
+      age: new FormControl("",[Validators.required,Validators.min(18)]),
+      gender: new FormControl("",[Validators.required]),
+      phone: new FormControl("",[Validators.required,Validators.pattern(/^\+84\d{9,10}$/)])
+    })
+  }
+
+  checkPass(control: AbstractControl){
+    let check = control.value;
+    if (check.password != check.confirmPassword ) {
+      return {NoMatch: true}
+    }
+    return null;
+  }
+
+  add() {
+  }
+}
